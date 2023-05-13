4c9cab6c9a30cc719106fd5018a484acd226711d
Harvey <canh.do@reddotlabs.com>
2023-05-13 15:53:17 +0700

    update

--- resources/views/layouts/includes/admin/navbar.blade.php
+++ resources/views/layouts/includes/admin/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="navbar-brand-wrapper d-flex justify-content-center">
         <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
-            <a class="navbar-brand brand-logo" href="index.html"><img src="{{ asset('admin/assets/images/logo.png') }}" alt="logo" /></a>
-            <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('admin/assets/images/logo-mini.svg') }}" alt="logo" /></a>
+            <a class="navbar-brand brand-logo" href="/admin/dashboard"><img src="{{ asset('admin/assets/images/logo.png') }}" alt="logo" /></a>
+            <a class="navbar-brand brand-logo-mini" href="/admin/dashboard"><img src="{{ asset('admin/assets/images/logo-mini.svg') }}" alt="logo" /></a>
             <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
                 <span class="mdi mdi-sort-variant"></span>
             </button>
@@ -118,20 +118,18 @@
             <li class="nav-item nav-profile dropdown">
                 <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
                     <img src="{{ asset('admin/assets/images/faces/face5.jpg') }}" alt="profile" />
+                    @if(Auth::guard('admin')->check())
                     <span class="nav-profile-name">{{Auth::guard('admin')->user()->name}}</span>
+                    @endif
                 </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
                     <a class="dropdown-item">
                         <i class="mdi mdi-settings text-primary"></i>
-                        Settings
+                        Cài đặt
                     </a>
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                        <i class="mdi mdi-logout text-primary"></i> {{ __('Logout') }}
+                    <a class="dropdown-item" href="{{ route('logout') }}">
+                        <i class="mdi mdi-logout text-primary"></i> {{ __('Đăng xuất') }}
                     </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
                 </div>
             </li>
         </ul>
--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -3,42 +3,42 @@
         <li class="nav-item">
             <a class="nav-link" href="{{ url('admin/dashboard') }}">
                 <i class="mdi mdi-view-dashboard menu-icon"></i>
-                <span class="menu-title">Dashboard</span>
+                <span class="menu-title">Trang chủ</span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" data-bs-toggle="collapse" href="#categories_menu" aria-expanded="false" aria-controls="categories_menu">
                 <i class="mdi mdi-package menu-icon"></i>
-                <span class="menu-title">Categories</span>
+                <span class="menu-title">Danh mục</span>
                 <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="categories_menu">
                 <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="{{ url('admin/new/category') }}">Add Category</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ url('admin/category') }}">View Categories</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{ url('admin/new/category') }}">Thêm danh mục</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{ url('admin/category') }}">Danh sách danh mục</a></li>
                 </ul>
             </div>
         </li>
         <li class="nav-item">
             <a class="nav-link" data-bs-toggle="collapse" href="#products_menu" aria-expanded="false" aria-controls="products_menu">
                 <i class="mdi mdi-movie menu-icon"></i>
-                <span class="menu-title">Products</span>
+                <span class="menu-title">Sản phẩm</span>
                 <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="products_menu">
                 <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="{{ url('admin/new/book') }}">Add Product</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ url('admin/book') }}">View Products</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{ url('admin/new/book') }}">Thêm sản phẩm</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{ url('admin/book') }}">Danh sách sản phẩm</a></li>
                 </ul>
             </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#products_menu" aria-expanded="false" aria-controls="products_menu">
+            <a class="nav-link" data-bs-toggle="collapse" href="#orders_menu" aria-expanded="false" aria-controls="orders_menu">
                 <i class="mdi mdi-movie menu-icon"></i>
                 <span class="menu-title">Quản lý đơn hàng</span>
                 <i class="menu-arrow"></i>
             </a>
-            <div class="collapse" id="products_menu">
+            <div class="collapse" id="orders_menu">
                 <ul class="nav flex-column sub-menu">
                     <li class="nav-item"><a class="nav-link" href="{{ url('admin/order') }}"><i class="fa fa-building"></i>Toàn bộ đơn hàng</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ url('admin/order_pending') }}"><i class="fa fa-hospital-o"></i>Đơn hàng đang chờ</a></li> 
@@ -49,41 +49,17 @@
             </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">
-                <i class="mdi mdi-cash-multiple menu-icon"></i>
-                <span class="menu-title">Sales</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-                <i class="mdi mdi-emoticon menu-icon"></i>
-                <span class="menu-title">Brands</span>
-            </a>
-        </li>
-        <li class="nav-item">
             <a class="nav-link" data-bs-toggle="collapse" href="#users_menu" aria-expanded="false" aria-controls="users_menu">
                 <i class="mdi mdi-account menu-icon"></i>
-                <span class="menu-title">Users</span>
+                <span class="menu-title">Người dùng</span>
                 <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="users_menu">
                 <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="#">Add User</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="#">View Users</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Thêm người dùng</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Danh sách người dùng</a></li>
                 </ul>
             </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-                <i class="mdi mdi-view-carousel menu-icon"></i>
-                <span class="menu-title">Home Slider</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-                <i class="mdi mdi-settings menu-icon"></i>
-                <span class="menu-title">Site Setting</span>
-            </a>
         </li>
     </ul>
 </nav>
